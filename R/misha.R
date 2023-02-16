57950625391a5a15295ceaf938b9a06c8f15e2b5
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2023-02-16 12:58:45 +0200

    Added gextract_pwm.quantile and gpwm_quantiles

---
+++ R/misha.R
@@ -0,0 +1,128 @@
+#' Extract pwm of intervals from a motif database
+#'
+#' @param intervals misha intervals set
+#'
+#' @return The intervals set with additional columns per motif, containing the pwm of each interval for each motif
+#'
+#' @inheritParams extract_pwm
+#'
+#' @examples
+#' \dontrun{
+#' library(misha)
+#' gdb.init_examples()
+#' pwms <- gextract_pwm("annotations")
+#' pwms[, 1:20]
+#' }
+#'
+#' @export
+gextract_pwm <- function(intervals, motifs = NULL, dataset = all_motif_datasets(), spat = NULL, spat_min = 0, spat_max = NULL, bidirect = TRUE, prior = 0, parallel = getOption("prego.parallel", TRUE)) {
+    if (!requireNamespace("misha", quietly = TRUE)) {
+        cli_abort("The {.field misha} package is required for this function. Please install it with {.code remotes::install_packages('tanaylab/misha')}.")
+    }
+
+    if (is.character(intervals)) {
+        intervals <- misha::gintervals.load(intervals)
+    }
+
+    sequences <- misha::gseq.extract(intervals)
+
+    res <- extract_pwm(sequences, motifs = motifs, dataset = dataset, spat = spat, spat_min = spat_min, spat_max = spat_max, bidirect = bidirect, prior = prior, parallel = parallel)
+
+    return(cbind(intervals, as.data.frame(res)))
+}
+
+#' Extract quantiles of pwm of intervals from a motif database
+#'
+#' @description Extract for each interval its quantile in the genome for each motif given its length. Note
+#' that the quantiles are computed for each motif separately, and therefore this might be slow for intervals with un-normalized lengths.
+#'
+#' @param intervals misha intervals set
+#' @param percision the percision of the quantiles. Default is 0.01, which means that the quantiles will be computed for every 1% of the pwm.
+#'
+#' @examples
+#' \dontrun{
+#' library(misha)
+#' gdb.init_examples()
+#' gextract_pwm.quantile("annotations", motifs = c("JASPAR.CDX1", "JASPAR.CDX2"), dist_from_edge = 100)
+#' }
+#'
+#' @inheritParams gextract_pwm
+#' @inheritParams gpwm_quantiles
+#'
+#' @export
+gextract_pwm.quantile <- function(intervals, motifs = NULL, dataset = all_motif_datasets(), percision = 0.01, spat = NULL, spat_min = 0, spat_max = NULL, bidirect = TRUE, prior = 0, n_sequences = 1e4, dist_from_edge = 3e6, chromosomes = NULL, parallel = getOption("prego.parallel", TRUE)) {
+    pwms <- gextract_pwm(intervals, motifs = motifs, dataset = dataset, spat = spat, spat_min = spat_min, spat_max = spat_max, bidirect = bidirect, prior = prior, parallel = parallel)
+
+    if (!is.null(motifs)) {
+        dataset <- dataset %>% filter(motif %in% motifs)
+    }
+
+    if (is.character(intervals)) {
+        intervals <- misha::gintervals.load(intervals)
+    }
+
+    pwms <- pwms %>%
+        mutate(l = end - start)
+    breaks <- seq(0, 1, by = percision)
+
+    if (length(unique(pwms$l)) > 1) {
+        cli_alert_warning("The length of the intervals is not the same. The computation of the quantiles will be done for each length separately which might take a while.")
+        cli_ul("Please consider using {.code misha.ext::gintervals.normalize} to normalize the intervals to the same length before running this function.")
+    }
+
+    # go over every length and compute quantiles
+    pwm_q <- plyr::ddply(pwms, "l", function(pwms_l) {
+        pwms_l <- tibble::as_tibble(pwms_l)
+
+        res <- plyr::daply(dataset, "motif", function(x) {
+            quantiles <- gpwm_quantiles(size = pwms_l$l[1], quantiles = breaks, pssm = x, n_sequences = n_sequences, dist_from_edge = dist_from_edge, chromosomes = chromosomes)
+            val2quant <- approxfun(x = quantiles, y = breaks, rule = 2)
+            val2quant(pwms_l[[x$motif[1]]])
+        }, .parallel = parallel)
+        res <- as.matrix(as.data.frame(t(res)))
+        res
+    })
+
+    pwm_q <- pwm_q %>%
+        select(-l)
+
+    return(cbind(intervals, as.data.frame(pwm_q)))
+}
+
+
+
+#' Compute quantile of pwm for a given interval size
+#'
+#' @description Computes the quantile of the pwm for a given interval size by sampling random intervals from the genome. The number of sequences to sample can be specified with \code{n_sequences}.
+#'
+#' @param size size of the intervals to sample
+#' @param pssm PSSM matrix or data frame
+#' @param quantiles quantiles to compute. See \code{quantile} for more details.
+#' @param n_sequences number of sequences to sample in order to compute the quantiles. The default is 1e4.
+#'
+#' @inheritParams compute_pwm
+#' @inheritParams misha.ext::grandom_genome
+#'
+#' @examples
+#' \dontrun{
+#' library(misha)
+#' library(dplyr)
+#' gdb.init_examples()
+#' pssm <- JASPAR_motifs %>%
+#'     filter(motif == "JASPAR.CDX1") %>%
+#'     select(-motif)
+#' gpwm_quantiles(1000, seq(0, 1, 0.1), pssm, dist_from_edge = 100)
+#' }
+#'
+#' @export
+gpwm_quantiles <- function(size, quantiles, pssm, spat = NULL, spat_min = 0, spat_max = NULL, bidirect = TRUE, prior = 0, n_sequences = 1e4, dist_from_edge = 3e6, chromosomes = NULL) {
+    if (!requireNamespace("misha.ext", quietly = TRUE)) {
+        cli_abort("The {.field misha.ext} package is required for this function. Please install it with {.code remotes::install_packages('tanaylab/misha.ext')}.")
+    }
+
+    r_intervals <- misha.ext::grandom_genome(size, n_sequences, dist_from_edge, chromosomes)
+    sequences <- misha::gseq.extract(r_intervals)
+    pwm <- compute_pwm(sequences, pssm, spat = spat, spat_min = spat_min, spat_max = spat_max, bidirect = bidirect, prior = prior)
+
+    return(quantile(pwm, probs = quantiles, na.rm = TRUE))
+}
