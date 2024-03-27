f6a0bcdd4521342a794e21a49d392dc199b6b721
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2024-03-27 10:36:19 +0200

    exported bits_per_pos

--- R/pssm-utils.R
+++ R/pssm-utils.R
@@ -156,6 +156,27 @@ validate_spat <- function(spat) {
     }
 }
 
+#' Calculate the number of bits per position in a Position-Specific Scoring Matrix (PSSM).
+#'
+#' This function takes a PSSM as input and calculates the number of bits per position.
+#' The PSSM should be a data frame or matrix with columns representing the nucleotides A, C, G, and T.
+#' The function first normalizes the PSSM by dividing each element by the sum of its row.
+#' Then, it calculates the entropy for each position using the formula: bits = log2(4) + sum(p * log2(p)),
+#' where p is the probability of each nucleotide at the position.
+#' Finally, it sets any negative values to zero and returns the resulting bits per position.
+#'
+#' @param pssm A data frame or matrix representing the Position-Specific Scoring Matrix (PSSM).
+#' @return A numeric vector representing the number of bits per position in the PSSM.
+#' @examples
+#' pssm <- data.frame(
+#'     A = c(0.2, 0.3, 0.1, 0.4),
+#'     C = c(0.1, 0.2, 0.3, 0.4),
+#'     G = c(0.4, 0.3, 0.2, 0.1),
+#'     T = c(0.3, 0.2, 0.4, 0.1)
+#' )
+#' bits_per_pos(pssm)
+#'
+#' @export
 bits_per_pos <- function(pssm) {
     pssm <- as.matrix(pssm[, c("A", "C", "G", "T")])
     pssm <- pssm / rowSums(pssm)
