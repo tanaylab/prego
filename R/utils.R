f305b1fd5ce24755f9cc1686d17fec262c05021b
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2026-02-22 09:17:57 +0200

    Prepare package for CRAN checks and add CI workflows

--- R/utils.R
+++ R/utils.R
@@ -10,9 +10,15 @@ is_binary_response <- function(response) {
     ncol(response) == 1 && all(response %in% c(0, 1))
 }
 
+is_pkg_installed <- function(pkg) {
+    nzchar(system.file(package = pkg))
+}
+
 #' Set parallel threads
 #'
-#' @description Set the number of parallel threads to use. prego uses the R function \code{doMC::registerDoMC} to register the parallelization.
+#' @description Set the number of parallel threads to use. prego uses
+#' \code{doMC::registerDoMC} (when available) to register the
+#' parallelization backend for \pkg{plyr}.
 #' By default, prego uses 80% of the number of available cores. The options are saved under 'prego.parallel' (should we use parallelization, logical) and 'prego.parallel.nc' (number of cores to use, integer).
 #'
 #' @param thread_num number of threads. use '1' for non parallel behavior
@@ -25,14 +31,27 @@ is_binary_response <- function(response) {
 #' }
 #' @export
 set_parallel <- function(thread_num = max(1, round(parallel::detectCores() * 0.8))) {
+    options(prego.parallel.nc = thread_num)
+    RcppParallel::setThreadOptions(numThreads = thread_num)
+
     if (thread_num <= 1) {
         options(prego.parallel = FALSE)
-    } else {
-        doMC::registerDoMC(thread_num)
-        options(prego.parallel = TRUE)
-        options(prego.parallel.nc = thread_num)
-        RcppParallel::setThreadOptions(numThreads = thread_num)
+        return(invisible(NULL))
     }
+
+    if (!requireNamespace("doMC", quietly = TRUE)) {
+        cli_warn(c(
+            "{.pkg doMC} is not installed.",
+            "i" = "Falling back to non-parallel {.pkg plyr} execution.",
+            "i" = "Install {.pkg doMC} on Unix-like systems to enable {.pkg plyr} parallelism."
+        ))
+        options(prego.parallel = FALSE)
+        return(invisible(NULL))
+    }
+
+    doMC::registerDoMC(thread_num)
+    options(prego.parallel = TRUE)
+    invisible(NULL)
 }
 
 safe_llply <- function(.data, .fun, ..., .parallel = FALSE) {
@@ -65,22 +84,6 @@ safe_ldply <- function(.data, .fun, ..., .parallel = FALSE, .id = NA) {
                 plyr::ldply(.data, .fun, ..., .parallel = FALSE, .id = .id)
             } else {
                 # If it's a different error, re-throw it
-                stop(e)
-            }
-        }
-    )
-}
-
-safe_ldply <- function(.data, .fun, ..., .parallel = FALSE, .id = NA) {
-    tryCatch(
-        {
-            plyr::ldply(.data, .fun, ..., .parallel = .parallel, .id = .id)
-        },
-        error = function(e) {
-            if (grepl("cannot allocate", e$message, ignore.case = TRUE)) {
-                warning("Memory allocation error detected. Falling back to non-parallel execution...")
-                plyr::ldply(.data, .fun, ..., .parallel = FALSE, .id = .id)
-            } else {
                 stop(e)
             }
         }
